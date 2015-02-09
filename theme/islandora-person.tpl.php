ffe4fd07170283352b1e2d3da8a956f3ab25fb0f
Matthew Short <mshort@niu.edu>
2015-02-09 11:04:40 -0600

    test
    test

--- theme/islandora-person.tpl.php
+++ theme/islandora-person.tpl.php
@@ -22,30 +22,19 @@
         <?php print $variables['activities']; ?>
       <?php endif; ?>
     </div>
-  </div>
-
-  <div class="col-right">
-    <div class="islandora-object-scholars">
-      <?php if (isset($variables['fellow_scholars'])): ?>
-        <?php foreach ($variables['fellow_scholars'] as $dept): ?>
-          <?php print $dept; ?>
-        <?php endforeach; ?>
-      <?php endif; ?>
-    </div>
-  </div>
-
-  <div class="islandora-object-content">
-    <h3 class="bio"><?php print t('Biography'); ?></h3>
-    <?php if (isset($variables['biography'])): ?>
-      <p><?php print $variables['biography']; ?></p>
-    <?php endif; ?>
-
+    <div class="islandora-object-citations">
     <?php if (isset($variables['recent_citations'])): ?>
       <?php print $variables['recent_citations']; ?>
     <?php endif; ?>
     <?php if (isset($variables['rss_feed'])): ?>
       <?php print $variables['rss_feed']; ?>
     <?php endif; ?>
-  </div>
-
+    </div>
+   </div>
+    <div class="islandora-object-biography">
+    <h3 class="bio"><?php print t('Biography'); ?></h3>
+    <?php if (isset($variables['biography'])): ?>
+      <p><?php print $variables['biography']; ?></p>
+    <?php endif; ?>
+    </div>
 </div>
