2ebd3ec46e4e9d6cb74cfaaa072c6e763972ed58
Matthew Short <mshort@niu.edu>
2015-02-16 12:53:25 -0600

    test

--- theme/islandora-person.tpl.php
+++ theme/islandora-person.tpl.php
@@ -6,7 +6,7 @@
 ?>
 <div class="islandora-object islandora">
 
-  <div class="col-left">
+  <div class="islandora-object-content">
     <div class="islandora-object-image">
       <?php if (isset($variables['tn'])): ?>
         <img src="<?php print $variables['tn']; ?>"/>
@@ -17,10 +17,37 @@
         <?php print $variables['metadata']; ?>
       <?php endif; ?>
     </div>
-    <div class="islandora-object-activities">
-      <?php if (isset($variables['activities'])): ?>
-        <?php print $variables['activities']; ?>
-      <?php endif; ?>
+     <div class="islandora-external-links islandora">
+        <h3 class="islandora-external-links"><?php print t('External Links'); ?></h3>
+        <div class="islandora-external-links-leblanc">
+            <?php if($wiki_page): ?>            
+            <a href="<?php print $variables['wiki_page']; ?>"><?php print t('Wikipedia'); ?></a>
+            <?php endif; ?>   
+        </div>
+        <div class="islandora-external-links-leblanc">
+        <?php if($leblanc): ?>            
+            <a href="<?php print $variables['leblanc']; ?>"><?php print t('Edward T. LeBlanc Bibliography'); ?></a>
+        <?php endif; ?>   
+        </div>
+                <div class="islandora-external-links-viaf">
+        <?php if($viaf): ?>            
+            <a href="<?php print $variables['viaf']; ?>"><?php print t('Virtual International Authority File'); ?></a>
+        <?php endif; ?>   
+        </div>
+    </div>
+	<div class="islandora-related-objects islandora">
+	   <div class="islandora-related-content-wrapper clearfix">
+	       <?php if($related): ?>
+	       <h3 class="bio"><?php print t('Related identities in the collection'); ?></h3>
+    	   <div>
+    	       <ul>
+    	       <?php foreach ($related as $obj): ?>
+    	           <li><?php print l($obj->label, "islandora/object/{$obj->id}"); ?></li>
+    	       <?php endforeach; ?>
+    	       </ul>
+           </div>
+           <?php endif; ?>
+       </div>
     </div>
     <div class="islandora-object-citations">
     <?php if (isset($variables['recent_citations'])): ?>
@@ -31,6 +58,11 @@
     <?php endif; ?>
     </div>
    </div>
+    <div class="islandora-object-wikipedia">
+    <?php if (isset($variables['abstract'])): ?>
+      <p><?php print $variables['abstract']; ?></p>
+    <?php endif; ?>
+    </div>
     <div class="islandora-object-biography">
     <h3 class="bio"><?php print t('Biography'); ?></h3>
     <?php if (isset($variables['biography'])): ?>
